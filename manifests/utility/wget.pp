03f5158f88ca68fdb0bc381bcec96d0c75be3b90
Ronan Chilvers <ronan@thelittledot.com>
2014-12-04 23:44:43 +0000

    Remove package dependency from wget

--- manifests/utility/wget.pp
+++ manifests/utility/wget.pp
@@ -4,11 +4,10 @@ define php::utility::wget(
   $package_name = 'wget',
   $binary       = '/usr/bin/wget',
 ){
-    $rand = fqdn_rand(100,$name)
-    package { "wget_package_$rand":
-        ensure => present,
-        name   => $package_name,
-    }
+    # package { "wget_package_$rand":
+    #     ensure => present,
+    #     name   => $package_name,
+    # }
 
     exec { "wget_$rand":
         command => "$binary -q -O $destination $download_url",
